1765993e6bd164064a4f1731f08a07a7fcc1ec52
Magnus Møller <magnus108@me.com>
2017-07-18 09:44:47 +0000

    maybe

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -278,7 +278,6 @@ $config_directories = array(
  *
  * @see install_select_profile()
  */
-$settings['install_profile'] = 'standard';
 
 /**
  * Salt for one-time login links, cancel links, form tokens, etc.
